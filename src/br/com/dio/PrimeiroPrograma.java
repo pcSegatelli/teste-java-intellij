90d6ad236d1e5cec136347bee863dd8044c39c4a
Paula Segatelli <pc.segatelli@gmail.com>
2023-05-21 00:35:27 -0300

    livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,6 +11,40 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("o problema dos 3 corpos", 300);
+        System.out.println(livro);
 
     }
 }
+    class Livro {
+        private String nome;
+        private int numeroPaginas;
+
+        public Livro(String nome, int numeroPaginas) {
+            this.nome = nome;
+            this.numeroPaginas = numeroPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getNumeroPaginas() {
+            return numeroPaginas;
+        }
+
+        public void setNumeroPaginas(int numeroPaginas) {
+            this.numeroPaginas = numeroPaginas;
+        }
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numeroPaginas=" + numeroPaginas +
+                    '}';
+        }
+    }
